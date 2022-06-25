0e08a3390273b1f0933068d2df0232aabf044047
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2022-06-25 22:10:22 +0700

    build: utilize `@run-z/project-config`

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,37 +1,6 @@
-import { externalModules } from '@run-z/rollup-helpers';
-import { defineConfig } from 'rollup';
-import flatDts from 'rollup-plugin-flat-dts';
-import sourcemaps from 'rollup-plugin-sourcemaps';
-import ts from 'rollup-plugin-typescript2';
-import typescript from 'typescript';
+import { ProjectConfig } from '@run-z/project-config';
+import { ProjectRollup } from '@run-z/project-config/rollup';
 
-export default defineConfig({
-  input: {
-    async: './src/mod.ts',
-  },
-  plugins: [
-    ts({
-      typescript,
-      tsconfig: 'tsconfig.main.json',
-      cacheRoot: 'target/.rts2_cache',
-    }),
-    sourcemaps(),
-  ],
-  external: externalModules(),
-  output: {
-    dir: 'dist',
-    format: 'esm',
-    sourcemap: true,
-    entryFileNames: '[name].js',
-    plugins: [
-      flatDts({
-        tsconfig: 'tsconfig.main.json',
-        lib: true,
-        file: 'async.d.ts',
-        compilerOptions: {
-          declarationMap: true,
-        },
-      }),
-    ],
-  },
-});
+const project = new ProjectConfig();
+
+export default new ProjectRollup(project).build();
