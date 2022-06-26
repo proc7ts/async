4a7715d1ab73a784cbcc11745a97d842b65e0db3
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2022-06-26 12:26:03 +0700

    chore(deps): `@run-z/project-config` v0.2.0

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,3 @@
-import { ProjectConfig } from '@run-z/project-config';
 import { ProjectRollup } from '@run-z/project-config/rollup';
 
-const project = new ProjectConfig();
-
-export default new ProjectRollup(project).build();
+export default ProjectRollup.build();
