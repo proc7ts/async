d7e3b0a3fa6ede0c84134a3fa2a2d297288d3b08
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-05-09 11:48:50 +0700

    Export `EveryPromiseResolver`

--- src/mod.ts
+++ src/mod.ts
@@ -1,7 +1,7 @@
 /**
- * @packageDocumentation
  * @module @proc7ts/async
  */
+export * from './every-promise-resolver.js';
 export * from './promise-resolver.js';
-export * from './semaphore.js';
 export * from './semaphore-revoke-error.js';
+export * from './semaphore.js';
